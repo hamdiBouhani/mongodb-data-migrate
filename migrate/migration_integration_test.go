2f729bc9f7f552d7a5e4df64186d293ca5995e19
hamdi bouhani <hamdi.bouhani@target-energysolutions.com>
2021-02-25 16:34:41 +0100

    add unit test migration

--- migrate/migration_integration_test.go
+++ migrate/migration_integration_test.go
@@ -105,3 +105,73 @@ func TestSetGetVersion(t *testing.T) {
 		return
 	}
 }
+
+func TestVersionBeforeSet(t *testing.T) {
+	defer cleanup(client)
+	migrate := NewMigrate(testDB, client)
+	version, _, err := migrate.Version()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+	if version != 0 {
+		t.Errorf("Unexpected version: %v", err)
+		return
+	}
+}
+
+func TestUpMigrations(t *testing.T) {
+	defer cleanup(client)
+	migrate := NewMigrate(testDB, client,
+		Migration{Version: 1, Description: "hello", Up: func(db *mongo.Client) error {
+			_collection := db.Database(testDB).Collection(testCollection)
+			_, err := _collection.InsertOne(context.Background(), bson.M{"hello": "world"})
+			if err != nil {
+				return err
+			}
+			return nil
+		}},
+		Migration{Version: 2, Description: "world", Up: func(db *mongo.Client) error {
+			//return db.C(testCollection).EnsureIndex(mgo.Index{Name: "test_idx", Key: []string{"hello"}})
+
+			_collection := db.Database(testDB).Collection(testCollection)
+			_, err := _collection.Indexes().CreateOne(context.Background(), mongo.IndexModel{
+				Keys: bson.M{
+					"hello": 1, // index in ascending order
+				}, Options: nil,
+			})
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}},
+	)
+	if err := migrate.Up(AllAvailable); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+
+	version, description, err := migrate.Version()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+	if version != 2 || description != "world" {
+		t.Errorf("Unexpected version/description %v %v", version, description)
+		return
+	}
+
+	doc := bson.M{}
+	_collection := client.Database(testDB).Collection(testCollection)
+	err = _collection.FindOne(context.Background(), bson.M{"hello": "world"}).Decode(&doc)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+
+	if doc["hello"].(string) != "world" {
+		t.Errorf("Unexpected data")
+		return
+	}
+}
