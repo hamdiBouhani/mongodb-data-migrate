67175ad811c55246c8c53cd56ba0eb6b70a07794
hamdi bouhani <hamdi.bouhani@target-energysolutions.com>
2021-02-25 17:12:28 +0100

    add unit test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/spf13/cobra v1.1.3
 	go.mongodb.org/mongo-driver v1.4.6
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
--- migrate/migration_integration_test.go
+++ migrate/migration_integration_test.go
@@ -133,12 +133,13 @@ func TestUpMigrations(t *testing.T) {
 		}},
 		Migration{Version: 2, Description: "world", Up: func(db *mongo.Client) error {
 			//return db.C(testCollection).EnsureIndex(mgo.Index{Name: "test_idx", Key: []string{"hello"}})
-
+			indexOptions := options.Index()
+			indexOptions.SetName("test_idx")
 			_collection := db.Database(testDB).Collection(testCollection)
 			_, err := _collection.Indexes().CreateOne(context.Background(), mongo.IndexModel{
 				Keys: bson.M{
 					"hello": 1, // index in ascending order
-				}, Options: nil,
+				}, Options: indexOptions,
 			})
 			if err != nil {
 				return err
@@ -174,4 +175,109 @@ func TestUpMigrations(t *testing.T) {
 		t.Errorf("Unexpected data")
 		return
 	}
+}
+
+func TestDownMigrations(t *testing.T) {
+	defer cleanup(client)
+
+	migrate := NewMigrate(testDB, client,
+		Migration{
+			Version:     1,
+			Description: "hello",
+			Up: func(db *mongo.Client) error {
+				_collection := db.Database(testDB).Collection(testCollection)
+				_, err := _collection.InsertOne(context.Background(), bson.M{"hello": "world"})
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			Down: func(db *mongo.Client) error {
+				_collection := db.Database(testDB).Collection(testCollection)
+				_, err := _collection.DeleteOne(context.Background(), bson.M{"hello": "world"})
+				if err != nil {
+					return err
+				}
+				return nil
+			}},
+		Migration{
+			Version:     2,
+			Description: "world",
+			Up: func(db *mongo.Client) error {
+				//return db.C(testCollection).EnsureIndex(mgo.Index{Name: "test_idx", Key: []string{"hello"}})
+				indexOptions := options.Index()
+				indexOptions.SetName("test_idx")
+				_collection := db.Database(testDB).Collection(testCollection)
+				_, err := _collection.Indexes().CreateOne(context.Background(), mongo.IndexModel{
+					Keys: bson.M{
+						"hello": 1, // index in ascending order
+					}, Options: indexOptions,
+				})
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+			Down: func(db *mongo.Client) error {
+				_collection := db.Database(testDB).Collection(testCollection)
+				_, err := _collection.Indexes().DropOne(context.Background(), "test_idx")
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
+		})
+	if err := migrate.Up(AllAvailable); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+	if err := migrate.Down(AllAvailable); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+	version, _, err := migrate.Version()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+	if version != 0 {
+		t.Errorf("Unexpected version: %v", version)
+		return
+	}
+
+	_collection := client.Database(testDB).Collection(testCollection)
+	err = _collection.FindOne(context.Background(), bson.M{"hello": "world"}).Decode(&bson.M{})
+	if err != mongo.ErrNoDocuments {
+		t.Errorf("Unexpected error: %v", err)
+		return
+
+	}
+
+	// cur, err := _collection.Indexes().List(context.Background())
+	// if err != nil {
+	// 	t.Errorf("Unexpected error: %v", err)
+	// 	return
+	// }
+	// for cur.Next(context.Background()) {
+	// 	index := bson.D{}
+	// 	cur.Decode(&index)
+	// 	log.Println(fmt.Sprintf("index found %v", index))
+	// }
+
+	/*
+			indexes, err := mongo.C(testCollection).Indexes()
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+			return
+		}
+		for _, index := range indexes {
+			if index.Name == "test_idx" {
+				t.Errorf("Index unexpectedly found")
+				return
+			}
+		}
+	*/
+
 }
